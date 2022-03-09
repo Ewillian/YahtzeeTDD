c87ba200f86507abafb42295551a6c8c7be28b49
CdiscountGui <guillaume.lecoq@cdiscount.com>
2022-03-09 19:05:02 +0100

    Should_Add_Ones_Score_Once Test Fail

--- YahtzeeTDD.Tests/UnitTest1.cs
+++ YahtzeeTDD.Tests/UnitTest1.cs
@@ -1,15 +1,23 @@
-using System;
+using System.Collections.Generic;
 
 using Xunit;
 
 namespace YahtzeeTDD.Tests
 {
-    public class UnitTest1
+    public class ScoreManagerTest
     {
         [Fact]
-        public void Test1()
+        public void Should_Add_Ones_Score_Once()
         {
+            // Arrange
+            var dicesValues = new List<int> { 1, 2, 3, 4, 5 };
+            var scoreManager = new ScoreManager(dicesValues);
 
+            // Act
+            var actual = scoreManager.Ones();
+
+            // Assert
+            Assert.Equal(1, actual);
         }
     }
 }
---
+++ YahtzeeTDD/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace YahtzeeTDD
+{
+    public class ScoreManager
+    {
+        public List<int> DicesValues;
+        public ScoreManager(List<int> dicesValues)
+        {
+            DicesValues = dicesValues;
+        }
+
+        public int Ones()
+        {
+            return 0;
+        }
+    }
+}
