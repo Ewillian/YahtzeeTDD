c9de53716b2bd0c0b9a3d1224c2ad5015655940c
CdiscountGui <guillaume.lecoq@cdiscount.com>
2022-03-10 19:28:15 +0100

    Should_Add_Four_Values_Three_Of_A_Kind Test Fail

--- YahtzeeTDD.Tests/ScoreManagerTest.cs
+++ YahtzeeTDD.Tests/ScoreManagerTest.cs
@@ -81,10 +81,32 @@ namespace YahtzeeTDD.Tests
         [InlineData(4, 5, 6)]
         [InlineData(5, 6, 1)]
         [InlineData(6, 1, 2)]
-        public void Should_Add_Values_Three_Of_A_Kind(int repeatedValue, int firstFillValue, int secondFillValue)
+        public void Should_Add_Three_Values_Three_Of_A_Kind(int repeatedValue, int firstFillValue, int secondFillValue)
         {
             // Arrange
             var dicesValues = new List<int> { repeatedValue, repeatedValue, firstFillValue, repeatedValue, secondFillValue };
+            var expected = dicesValues.Sum();
+
+            var scoreManager = new ScoreManager(dicesValues);
+
+            // Act
+            var actual = scoreManager.ThreeOfAKind();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        [InlineData(3, 4)]
+        [InlineData(4, 5)]
+        [InlineData(5, 6)]
+        [InlineData(6, 1)]
+        public void Should_Add_Four_Values_Three_Of_A_Kind(int repeatedValue, int fillValue)
+        {
+            // Arrange
+            var dicesValues = new List<int> { repeatedValue, repeatedValue, repeatedValue, repeatedValue, fillValue };
             var expected = dicesValues.Sum();
 
             var scoreManager = new ScoreManager(dicesValues);
